986a9b9a5b874e8b5939b9b5a51fb86741250a2b
Jai Hind <i0110100001101001i@gmail.com>
2025-09-13 23:29:29 +0530

    Create flutter.dart

---
+++ flutter.dart
@@ -0,0 +1,33 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'screens/home_screen.dart';
+import 'services/bus_service.dart';
+import 'services/location_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => BusService()),
+        ChangeNotifierProvider(create: (_) => LocationService()),
+      ],
+      child: MaterialApp(
+        title: 'City Bus & Tourism',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
